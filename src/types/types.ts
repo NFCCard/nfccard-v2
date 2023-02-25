7c513760ef22aee517c3093badfac381404a7042
alihoushngi <62546675+alihoushngi@users.noreply.github.com>
2023-02-25 22:09:56 +0330

    changeesss

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,20 +8,4 @@ export interface ApplicationRootState {
     readonly localization: LocalizationProviderState;
 }
 
-export interface register {
-    username: string;
-    name_fa: string;
-    name_en: string;
-    des_fa: string;
-    des_en: string;
-    phone?: number | string;
-    email: string;
-    password: string;
-    role: number;
-    job: string;
-    avatar?: Blob | string;
-    socials?: [name: string, link: string] | string;
-    resume?: Blob | string;
-}
-
 export interface User {}
