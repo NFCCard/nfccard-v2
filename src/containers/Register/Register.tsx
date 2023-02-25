9241606375da7fdd354bb99224bd787280a4bc4c
alihoushngi <62546675+alihoushngi@users.noreply.github.com>
2023-02-25 22:10:31 +0330

    changeesssss

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,4 +1,4 @@
-import RegisterForm from "@components/Shared/Forms/Register/Register";
+import RegisterForm from "@components/Shared/Forms/RegisterForm/RegisterForm";
 import React from "react";
 import { StyledRegisterPageContainer } from "./StyleRegister";
 
