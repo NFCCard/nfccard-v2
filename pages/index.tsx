dcd10a38e413882a992c6dfb375fef09e59a2ed4
MamadK9 <mohammad.kp8093@gmail.com>
2023-02-26 22:17:28 +0330

    font added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,11 @@ import Modal from "@base/Modal/Modal";
 import Home from "@containers/Home/Home";
 import Layout from "@components/Shared/Layout/Layout";
 
-function HomePage() {
+export default function HomePage() {
     const [open, setOpen] = useState(false);
     return (
         <main>
             <Home />
-            <button onClick={() => setOpen(true)}>Click</button>
-            <Modal
-                isOpen={open}
-                onClose={() => setOpen(false)}
-                animation="slide-down"
-                closeOnBackDropClick
-            >
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Architecto in vero sunt eligendi, assumenda provident amet omnis
-                tempora veritatis fugiat nobis. Sit aliquam ab blanditiis vitae
-                hic nulla reprehenderit minus?
-            </Modal>
         </main>
     );
 }
